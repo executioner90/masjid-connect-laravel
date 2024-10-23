8ed8f0692a33543ca7e4e3336632e3dd47a6ab21
mohammad <mohammad@inktweb.nl>
2024-10-23 21:46:41 +0200

    mosque activities table initiate

---
+++ app/Models/Mosque/Activity.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Mosque;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Activity extends Model
+{
+    /** @use HasFactory<\Database\Factories\Mosque\ActivityFactory> */
+    use HasFactory;
+}
